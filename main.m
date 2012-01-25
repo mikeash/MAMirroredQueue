f373a52329870d484eba09f2318d1f1776795945
Michael Ash <mikeash@MikeAir.local>
2012-01-24 20:56:59 -0500

    initial commit

---
+++ main.m
@@ -0,0 +1,11 @@
+// clang -framework Foundation -W -Wall -Wno-unused-parameter main.m AllocatePair.c
+
+#import <Cocoa/Cocoa.h>
+
+#import "AllocatePair.h"
+
+
+int main(int argc, char **argv)
+{
+    test_allocate_pair();
+}
