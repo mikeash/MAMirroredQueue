3d5ad80806a3c222dc9c0496c7ee9d8759acd7ce
Michael Ash <mikeash@MikeAir.local>
2012-01-26 18:15:41 -0500

    add thread safety test, does not pass yet

--- MAMirroredQueue.m
+++ MAMirroredQueue.m
@@ -136,6 +136,64 @@ static void check_equal(MAMirroredQueue *queue, NSData *auxQueue)
             fail("bytes don't match, %d != %d at index %lu", buf1[i], buf2[i], (long)i);
 }
 
++ (void)testThreaded
+{
+    unsigned short seed[3] = { 0 };
+    
+    for(int iter = 0; iter < 100; iter++)
+    {
+        unsigned short *seedPtr1 = (unsigned short[]) { nrand48(seed), nrand48(seed), nrand48(seed) };
+        unsigned short *seedPtr2 = (unsigned short[]) { nrand48(seed), nrand48(seed), nrand48(seed) };
+        
+        NSUInteger targetLength = nrand48(seed) % 1024 * 1024 + 1;
+        
+        MAMirroredQueue *queue = [[MAMirroredQueue alloc] init];
+        
+        NSMutableData *inData = [NSMutableData data];
+        NSMutableData *outData = [NSMutableData data];
+        
+        dispatch_group_t group = dispatch_group_create();
+        
+        dispatch_group_async(group, dispatch_get_global_queue(0, 0), ^{
+            while([inData length] < targetLength)
+            {
+                unsigned len = nrand48(seedPtr1) % 1024 + 1;
+                unsigned remaining = targetLength - [inData length];
+                len = MIN(len, remaining);
+                
+                char buf[len];
+                for(unsigned i = 0; i < len; i++)
+                    buf[i] = nrand48(seedPtr1);
+                
+                [queue ensureWriteSpace: len];
+                memcpy([queue writePointer], buf, len);
+                [queue advanceWritePointer: len];
+                
+                [inData appendBytes: buf length: len];
+            }
+        });
+        dispatch_group_async(group, dispatch_get_global_queue(0, 0), ^{
+            while([outData length] < targetLength)
+            {
+                unsigned len = nrand48(seedPtr2) % 10240 + 1;
+                unsigned available = [queue availableBytes];
+                len = MIN(len, available);
+                
+                if(len > 0)
+                {
+                    [outData appendBytes: [queue readPointer] length: len];
+                    [queue advanceReadPointer: len];
+                }
+            }
+        });
+        
+        dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+        
+        if(![inData isEqual: outData])
+            fail("Datas not equal!");
+    }
+}
+
 + (void)runTests
 {
     unsigned short seed[3] = { 0 };
